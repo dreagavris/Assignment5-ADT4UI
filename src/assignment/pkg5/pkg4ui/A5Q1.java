ede2382dec716dc2615e38529bd2dbb353289e21
manok5757 <manok5757@HRH-IC0027319.wrdsb.ca>
2017-11-02 09:59:27 -0400

    A5Q1

---
+++ src/assignment/pkg5/pkg4ui/A5Q1.java
@@ -0,0 +1,138 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment.pkg5.pkg4ui;
+
+/**
+ *
+ * @author manok5757
+ */
+public class A5Q1 {
+
+    private IntNode head;
+    private int numItems;
+
+    public A5Q1() {
+        head = null;
+        numItems = 0;
+    }
+
+    public void add(int num) {
+        // start at beginning of list
+        IntNode node = head;
+        // see if it is first item
+        if (node == null) {
+            IntNode temp = new IntNode(num);
+            head = temp;
+        } else {
+            // travel to the end 
+            while (node.getNext() != null) {
+                // travel to the next node
+                node = node.getNext();
+            }
+            // node is the last node in the list
+            // create the new node
+            IntNode temp = new IntNode(num);
+            // insert it into the list
+            node.setNext(temp);
+        }
+        // increase the size counter 
+        numItems++;
+    }
+
+    public void add(int pos, int num) {
+        // at the front
+        if (pos == 0) {
+            // create the node
+            IntNode temp = new IntNode(num);
+            // the new node points to the start
+            temp.setNext(head);
+            // reassign the head node
+            head = temp;
+            // adding at the end of the list
+        } else if (pos == numItems) {
+            add(num);
+            // anywhere else . . .
+        } else {
+            // start at the beginning
+            IntNode node = head;
+            // move to the node before the insert
+            for (int i = 0; i < pos - 1; i++) {
+                node = node.getNext();
+            }
+            // @ node just before insert
+            IntNode temp = new IntNode(num);
+            // create the after linik first!
+            temp.setNext(node.getNext());
+            // change node pointer
+            node.setNext(temp);
+        }
+        // increase items in counter
+        numItems++;
+
+
+    }
+
+    public int size() {
+        return numItems;
+    }
+
+    public boolean isEmpty() {
+        return numItems == 0;
+    }
+
+    public int get(int position) {
+        IntNode node = head;
+        // move the number of times
+        for (int i = 0; i < position; i++) {
+            node = node.getNext();
+        }
+        return node.getNum();
+    }
+
+    public void remove(int pos) {
+
+        // if removing from the start
+        if (pos == 0) {
+            head = head.getNext();
+        } else if (pos == numItems - 1) {
+            // removing from end
+            IntNode node = head;
+            // moving to the second last item
+            for (int i = 0; i < numItems - 1; i++) {
+                node = node.getNext();
+            }
+            // sever the link
+            node.setNext(null);
+        } else {
+            IntNode node = head;
+            // move to the spot just before
+            for (int i = 0; i < pos - 1; i++) {
+                node = node.getNext();
+            }
+            // the node to remove
+            IntNode toRemove = node.getNext();
+            // its next node
+            IntNode next = toRemove.getNext();
+            // set all the llinks
+            node.setNext(next);
+            toRemove.setNext(null);
+        }
+        numItems--;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        A5Q1 list = new A5Q1();
+        list.add(2);
+        list.add(-5);
+        list.add(0, 13);
+        list.remove(1);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+}
---
+++ src/assignment/pkg5/pkg4ui/IntNode.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment.pkg5.pkg4ui;
+
+/**
+ *
+ * @author manok5757
+ */
+public class IntNode {
+
+    private int item;
+    private IntNode next;
+
+    public IntNode(int num) {
+        item = num;
+        next = null;
+
+    }
+
+    /**
+     * Returns the number stored in the node
+     *
+     * @return the stored number
+     */
+    public int getNum() {
+        return item;
+    }
+
+    /**
+     * Returns the node that comes after
+     *
+     * @return the next node
+     */
+    public IntNode getNext() {
+        return next;
+    }
+
+    /**
+     * Sets the node that comes after
+     *
+     * @param node the node to point to
+     */
+    public void setNext(IntNode node) {
+        next = node;
+    }
+}
