50cc09a1a09538ff8346e75635265b8f3dfeec94
Kiran <Kiran@192.168.0.202>
2017-11-06 22:45:33 -0500

    a5

--- src/assignment/pkg5/pkg4ui/A5Q1.java
+++ src/assignment/pkg5/pkg4ui/A5Q1.java
@@ -23,10 +23,9 @@ public class A5Q1 {
         IntNode node = head;
         // create a node to keep track of the node behind the primary node
         IntNode before = head;
-        
-        
-        
-        
+        boolean endSearch = false;
+        int counter = 1;
+
         // if the list is empty
         if (numItems == 0) {
             // add a node into the list containing the desired number
@@ -58,38 +57,48 @@ public class A5Q1 {
             }
         } // there is more than one item in the list
         else {
-            
-            node = node.getNext();
-            // move the primary comparison node over one
-          
-            for (int i = 0; i < numItems; i++) {
-                if (node.getNum() < num) {
-                    
-                    // move on to the next node in the list
-                    node = node.getNext();
-                    // move the before node to the next node
-                    before = before.getNext();
-                } // the number in the node is smaller or equal to the number to be entered
-                else {
-                    // create a new node to insert the number to be added
-                    IntNode temp = new IntNode(num);
-                    // link the new node created to the primary comparison node
-                    temp.setNext(node);
-                    // link the before node to the new temporary node
-                    before.setNext(temp);
-                    
+            System.out.println("fuc");
+
+            if (head.getNum() >= num) {
+                IntNode temp = new IntNode(num);
+                temp.setNext(head);
+                head = temp;
+            } else {
+
+                node = node.getNext();
+                System.out.println("shi");
+                // move the primary comparison node over one
+                while (!endSearch) {
+                    System.out.println("hi");
+                    counter++;
+                    System.out.println("em");
+                    if (node.getNum() >= num) {
+// create a new node to insert the number to be added
+                        IntNode temp = new IntNode(num);
+                        // link the new node created to the primary comparison node
+                        temp.setNext(node);
+                        // link the before node to the new temporary node
+                        before.setNext(temp);
+                        endSearch = true;
+                        
+                    } // the number in the node is smaller or equal to the number to be entered
+                    else if (node.getNum() < num) {
+                        // move on to the next node in the list
+                        node = node.getNext();
+                        System.out.println("hee");
+                        // move the before node to the next node
+                        before = before.getNext();
+                    } else if (counter == numItems) {
+                        IntNode temp = new IntNode(num);
+                        node.setNext(temp);
+                        endSearch = true;
+
+                    }
                 }
+
             }
         }
-
-
-
-
-
-
-
-
-        // increase the size counter 
+        // increase the size counter
         numItems++;
     }
 
@@ -181,22 +190,11 @@ public class A5Q1 {
     public static void main(String[] args) {
         // TESTING
         A5Q1 list = new A5Q1();
-        list.add(2);
-        list.add(210);
-        list.add(12);
         list.add(1);
-        list.add(111);
-
-        list.add(22);
-        list.add(12);
-        list.add(32);
-
-
-        list.add(1);
-
-        list.remove(2);
-
-
+        list.add(4);
+        list.add(9);
+            
+        list.add(0);
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
         }
